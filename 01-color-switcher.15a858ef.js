faff5bd6df139a11ebd32088fbad969f55a844e9
satanolia <132496844+satanolia@users.noreply.github.com>
2023-11-09 20:13:27 +0000

    Deploying to gh-pages from @ satanolia/goit-js-hw-09@6ec44d9830a7c433523ef9c30db40c961ee473ed 🚀

---
+++ 01-color-switcher.15a858ef.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");e.addEventListener("click",(function(){t=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.backgroundColor=t}),1e3),e.disabled=!0})),a.addEventListener("click",(function(){clearInterval(t),e.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.15a858ef.js.map
